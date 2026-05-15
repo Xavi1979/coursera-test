3424c7f2ed83f83b9f979779f8bb989fec2ab316
Xavi <xconejero@gmail.com>
2026-05-15 23:38:45 +0200

    Lecuture 6 finish

--- Lecture06/app.js
+++ Lecture06/app.js
@@ -4,19 +4,19 @@
 angular.module('NameCalculator', [])
 
 .controller('NameCalculatorController', function ($scope) {
-    $scope.name = "Xavi";
-    var totalNamevalue = calculatNumericForString($scope.name); 
-    $scope.totalValue = totalNameValue;    
+    $scope.name = "";
+    $scope.totalValue = 0;    
 
     $scope.displayNumeric = function(){        
-        var totalStringValue = 0;
-        $scope.totalStringValue = totalNamevalue;        
+        var totalNameValue = calculatNumericForString($scope.name);  
+        $scope.totalValue = totalNameValue;
+              
     };
 
     function calculatNumericForString(string){
         var totalStringValue = 0;
         for (var i=0; i < string.length; i++){
-            totalNameValue += string.charCodeAt(i);
+            totalStringValue += string.charCodeAt(i);
         }
 
         return totalStringValue;    
